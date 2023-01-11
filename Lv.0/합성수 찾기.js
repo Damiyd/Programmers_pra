a6789e6dadac54d65defc0a6c6a6227707f889c2
Damiyd <dpekfa144@gmail.com>
2023-01-11 22:02:05 +0900

    feat: 54번째 문제 완료

---
+++ Lv.0/합성수 찾기.js
@@ -0,0 +1,22 @@
+// 문제 설명
+// 약수의 개수가 세 개 이상인 수를 합성수라고 합니다. 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ n ≤ 100
+// 입출력 예
+// n	result
+// 10	5
+// 15	8
+
+function solution(n) {
+    var answer = new Set();
+    
+    for (let i = 1; i <= n; i++) { 
+        for (let j = 2; j < i; j++) 
+        if (i % j === 0) {  
+            answer.add(i)
+        }
+    }
+    
+    return answer.size;
+}
